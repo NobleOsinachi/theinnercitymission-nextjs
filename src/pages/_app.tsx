9a80eadc71b4726dd6c0f057bc96180caafc5f3b
Noble Osinachi <nobleosinachi98@gmail.com>
2023-07-10 17:18:23 +0100

    Include type checking for App component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 import "../styles/global.css";
 import { useRouter } from "next/router";
 
-export default function App({ Component, pageProps }) {
+type AppProps = {
+    Component: React.ComponentType;
+    pageProps: any; // Replace 'any' with the specific type of your 'pageProps' if available
+};
 
+export default function App({ Component, pageProps }: AppProps) {
     const router = useRouter();
 
     return (
