572e444368432f5fd0e272b3a4b16dd6052a1419
Noble Osinachi <nobleosinachi98@gmail.com>
2023-08-03 15:04:15 +0100

    Fix logo not displaying

--- components/IcmLogo.tsx
+++ components/IcmLogo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const IcmLogo = () => {
     return (
-        <Image src={"/logo.png"} alt={"icm logo"} width={100} height={100} />
+        <Image src={"/images/logo.png"} alt={"icm logo"} width={100} height={100} />
     );
 };
 
