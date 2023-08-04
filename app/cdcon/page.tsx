1a84ee775cb489c3c46e9a2afa4f05601259d592
Noble Osinachi <nobleosinachi98@gmail.com>
2023-08-04 06:51:50 +0100

    Remove hooks, made comp0nent async

--- app/cdcon/page.tsx
+++ app/cdcon/page.tsx
@@ -21,98 +21,83 @@ const Cdcon: React.FC = () => {
 
     const [userProfile, setUserProfile] = useState<UserProfile | null>(null);
 
+    // Function to fetch user profile
+    const getUserProfile = async (): Promise<boolean> => {
+        try {
+            const loginOptions: loginOptionsI = {
+                scopes: [
+                    "send_chat_message",
+                    // "conference_calls"
+                ],
+                clientId: "28b0b667-922f-4f15-8c61-a9a3622b3555"
+            };
 
+            const authenticationTokenResponse: authenticationTokenResponseI = await kingsChatWebSdk.login(loginOptions);
 
+            // Replace this with your actual client ID and Access Token
+            // const clientID = "28b0b667-922f-4f15-8c61-a9a3622b3555";
+            const accessToken = authenticationTokenResponse.accessToken;
+
+            // Endpoint URL for getting user profile
+            const profileURL = "https://connect.kingsch.at/developer/api/profile";
+
+            // Header with Authorization Bearer token
+            const headers = {
+                authorization: `Bearer ${accessToken}`,
+            };
+
+            const response = await fetch(profileURL, {
+                method: "GET",
+                headers: headers,
+            });
+
+            if (!response.ok) {
+                throw new Error("Request failed");
+            }
+
+            const data = await response.json();
+            setUserProfile(data.profile);
+            console.log("User Profile:", userProfile);
+
+            return true;
+        } catch (error) {
+            console.error("Error fetching user profile:", error);
+            return false;
+        }
+    };
+
+    // Call the function to fetch user profile immediately
     useEffect(() => {
-        const loginOptions: loginOptionsI = {
-            scopes: [
-                "send_chat_message",
-                // "conference_calls"
-            ],
-            clientId: "28b0b667-922f-4f15-8c61-a9a3622b3555"
-        };
-
-        // Function to fetch user profile
-        const getUserProfile = async () => {
-            try {
-                const authenticationTokenResponse: authenticationTokenResponseI = await kingsChatWebSdk.login(loginOptions);
-
-                // Replace this with your actual client ID and Access Token
-                // const clientID = "28b0b667-922f-4f15-8c61-a9a3622b3555";
-                const accessToken = authenticationTokenResponse.accessToken;
-
-                // Endpoint URL for getting user profile
-                const profileURL = "https://connect.kingsch.at/developer/api/profile";
-
-                // Header with Authorization Bearer token
-                const headers = {
-                    authorization: `Bearer ${accessToken}`,
-                };
-
-                const response = await fetch(profileURL, {
-                    method: "GET",
-                    headers: headers,
-                });
-
-                if (!response.ok) {
-                    throw new Error("Request failed");
-                }
-
-                const data = await response.json();
-                setUserProfile(data.profile);
-                console.log("User Profile:", userProfile);
-
-
-
-            } catch (error) {
-                console.error("Error fetching user profile:", error);
-            }
-        };
-
         getUserProfile();
+    }, []);
 
+    // Redirect to the desired URL after fetching the user profile
+    useEffect(() => {
+        if (userProfile) {
+            const kc_data = {
+                kc_firstname: userProfile.name.split(" ")[0],
+                kc_lastname: userProfile.name.split(" ")[1],
+                kc_email: (userProfile.email === null) ? "" : userProfile.email,
+                kc_phone_number: userProfile.phone_number,
+                kc_avatarUrl: userProfile.avatar
+            };
 
+            const urlParams = new URLSearchParams(Object.entries(kc_data)).toString();
 
-        useEffect(() => {
-            if (userProfile) {
-                const newData = {
-                    kc_firstname: userProfile.name.split(" ")[0],
-                    kc_lastname: userProfile.name.split(" ")[1],
-                    kc_email: (userProfile.email === null) ? "" : userProfile.email,
-                    kc_phone_number: userProfile.phone_number,
-                    kc_avatarUrl: userProfile.avatar
-                };
-
-                const urlParams = new URLSearchParams(Object.entries(newData)).toString();
-
-                // Use router.push here instead of in the previous useEffect
-                router.push(
-                    `http://localhost/icm4c/?page_id=15&openRegModal=true&${urlParams}`
-                );
-            }
-        }, []);
-
+            router.push(
+                `http://localhost/icm4c/?page_id=15&openRegModal=true&${urlParams}`
+                // `http://theInnerCityMision.ngo/CDCON/?openRegModal=true&${urlParams}`
+            );
+        }
     }, [userProfile, router]);
 
-
-
-
-    return (<div> {/* {userDetails && ( <div> <p>Access Token: {userDetails?.accessToken}</p> <p>Expires In (ms): {userDetails?.expiresInMillis}</p> <p>Refresh Token: {userDetails?.refreshToken}</p> </div> )} */}
-        <code className="w-100 m-auto">
-            {/* {userProfile && JSON.stringify(userProfile)} */}
-            {!userProfile && <i>Kindly enable Pop up...</i>}
-            {userProfile && <i>Redirecting...</i>}
-        </code>
-    </div>
+    return (
+        <div>
+            <code className="w-100 m-auto">
+                {userProfile && <i>Redirecting...</i>}
+            </code>
+        </div>
     );
 };
 
 export default Cdcon;
-
-/*
-
-https://healingstreams.tv/login.php
-check their implementation
-https://accounts.kingsch.at/?client_id=com.kingschat&scopes=%5B%22conference_calls%22%5D&post_redirect=true&redirect_uri=https://healingstreams.tv/kclogin.php
-
-*/
